d4111c248cb1673624cd1c46d2d0a610203b3ada
Madhav Sharma <103190887+i-madhav@users.noreply.github.com>
2024-06-14 20:21:07 +0530

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,22 +1,32 @@
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=225)
     price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2048)
 
+    def __str__(self):
+        return self.name
 
 class Offer(models.Model):
     code = models.CharField(max_length=10)
     description = models.CharField(max_length=255)
     discount = models.FloatField()
 
+    def __str__(self):
+        return self.code
+
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart {self.id}"
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1) 
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.product.name} in cart {self.cart.id}"
