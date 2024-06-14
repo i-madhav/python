0ec625ea54de38056983e28d94b5a5c8a7846789
Madhav Sharma <103190887+i-madhav@users.noreply.github.com>
2024-06-14 20:22:16 +0530

    Update views.py

--- products/views.py
+++ products/views.py
@@ -1,28 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Cart, CartItem, Product
-# Create your views here.
-
 
 def index(request):
     products = Product.objects.all()
-    return render(request , 'index.html' , 
-                  {'products':products}
-                  )
+    return render(request, 'index.html', {'products': products})
 
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     cart, created = Cart.objects.get_or_create()
 
-    try:
-        cart_item = CartItem.objects.get(cart=cart, product=product)
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not created:
         cart_item.quantity += 1
-        cart_item.save()
-    except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(
-            cart=cart,
-            product=product,
-            quantity=1
-        )
+    cart_item.save()
 
     return redirect('index')
+
+def view_cart(request):
+    cart = Cart.objects.first()  # This assumes a single cart for simplicity
+    cart_items = CartItem.objects.filter(cart=cart)
+    return render(request, 'cart.html', {'cart_items': cart_items})
