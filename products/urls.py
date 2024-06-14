d060055b4df81f1d94a5df9455b2e5a097214f46
Madhav Sharma <103190887+i-madhav@users.noreply.github.com>
2024-06-14 20:23:01 +0530

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,8 @@
-# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('view_cart/', views.view_cart, name='view_cart'),
 ]
